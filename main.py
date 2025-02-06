b8e013661083f06b82f363709b7d45b427e80bba
SuperGoldenMan <seananderson10@hotmail.co.uk>
2025-02-06 23:49:03 +0000

    added collisions

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #imports
+import sys
 import pygame
 from constants import *
 from player import *
 from circleshape import *
+from asteroid import *
+from asteroidfield import *
 
 #set the screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-
 
 #main function
 def main():
@@ -17,6 +18,18 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+#create groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+#add to groups
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+#create asteroid field
+    asteroidfield = AsteroidField()
 
 #create variable initialising game clock
     clock = pygame.time.Clock()
@@ -32,10 +45,18 @@ def main():
                 return
 #colour the screen
         screen.fill(color=(0,0,0))
-#draw the player each frame
-        player.draw(screen)
+
 #player update method hook
-        player.update(dt)
+        updatable.update(dt)
+#collision check
+        for roid in asteroids:
+            if roid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+#draw the player each frame
+        for item in drawable:
+            item.draw(screen)
+            
 #Update the full display surface to the screen
         pygame.display.flip()
 #set the frame rate to 60 fps
