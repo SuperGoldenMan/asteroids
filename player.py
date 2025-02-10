9c1b086ead8fb97581be74fbd9dc3c799fedd558
SuperGoldenMan <seananderson10@hotmail.co.uk>
2025-02-10 01:01:50 +0000

    added player acceleration and assets folder

--- player.py
+++ player.py
@@ -9,6 +9,11 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.shoot_timer = 0
+        self.velocity = pygame.Vector2(0, 0)
+        self.acceleration = pygame.Vector2(0, 0)
+        self.max_speed = PLAYER_MAX_SPEED
+        self.thrust = PLAYER_THRUST
+        self.friction = PLAYER_FRICTION
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -23,6 +28,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         self.shoot_timer -= dt
+        self.acceleration = pygame.Vector2(0, 0)
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -36,6 +42,18 @@ class Player(CircleShape):
         if keys[pygame.K_SPACE]:
             self.shoot()
 
+    # Apply acceleration to velocity
+        self.velocity += self.acceleration * dt
+    # Apply friction
+        self.velocity *= self.friction
+    # Limit speed
+        if self.velocity.length() > self.max_speed:
+            self.velocity.scale_to_length(self.max_speed)
+    # Update position based on velocity
+        self.position += self.velocity * dt
+    # Wrap position around screen
+        self.wrap_position()
+
     def shoot(self):
         if self.shoot_timer > 0:
             return
@@ -48,4 +66,19 @@ class Player(CircleShape):
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
+        if dt > 0:  # Moving forward (W key)
+            self.acceleration = forward * self.thrust
+        else:  # Braking (S key)
+            self.acceleration = -self.velocity * 2  # This will brake in the opposite direction of current movement
+
+# Screen Wrapping
+    def wrap_position(self):
+        if self.position.x < 0:
+            self.position.x = SCREEN_WIDTH
+        elif self.position.x > SCREEN_WIDTH:
+            self.position.x = 0
+            
+        if self.position.y < 0:
+            self.position.y = SCREEN_HEIGHT
+        elif self.position.y > SCREEN_HEIGHT:
+            self.position.y = 0
